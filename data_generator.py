3a243e2e93c0f524289dda35dbaef7baeed27636
Adam Mikolajczak <adammikolajczak2@gmail.com>
2023-02-28 00:14:52 +0100

    Added random data generator to provide data for testing

---
+++ data_generator.py
@@ -0,0 +1,6 @@
+import datetime
+import random
+
+products = {1: {'name': 'rice1', 'product': 'rice', 'price': 2.1, 'categoty': 'food', 'shop': 'lidl'},
+            2: {'name': 'rice2', 'product': 'rice', 'price': 2.2, 'categoty': 'food', 'shop': 'dino'},
+            }
